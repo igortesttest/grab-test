20d03521fec655f8464949eb70276e34448ade1d
valentin.belousov <belousowork@gmail.com>
2024-11-30 14:48:55 +0400

    Added cookies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import Cookies from 'js-cookie';
 import './App.css';
 import { useEffect, useRef } from 'react';
 
@@ -7,6 +7,11 @@ function App() {
   
   useEffect(() => { 
     if(frame.current){
+      Cookies.set('_ym_uid', '1732871902277884330', { expires: 7 });
+      Cookies.set('_ym_d', '1732871902', { expires: 7 });
+      Cookies.set('uid', 'am0bH1wdJ*JsQjhzIKw*LqZaefpffmCf', { expires: 7 });
+      Cookies.set('_ym_isad', '1', { expires: 7 });
+
       frame.current.contentWindow.location.href = 'https://hqq.tv/player/embed_player.php?vid=oCKZSB24mqzk&autoplay=none'
       console.log('kek')
     }
